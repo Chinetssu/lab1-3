7e9763c8713b5c1044f584ed6ee0d6873a1106af
sanad <sanad@CHINETSU>
2022-09-12 13:10:56 +0800

    Добавьте файлы проекта.

---
+++ lab1-3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleAppOnlyDigit
+{
+    public class Logic
+    {
+        public static int Convert(string exp)
+        {
+            int res = 0;
+            int num = 0;
+            bool neg = false;
+            foreach (char el in exp)
+            {
+                if (el.Equals('+'))
+                {
+                    res += num;
+                    neg = false;
+                    num = 0;
+                }
+                else
+                {
+                    if (el.Equals('-'))
+                    {
+                        res += num;
+                        neg = true;
+                        num = 0;
+                    }
+                    else
+                    {
+                        if (neg)
+                        {
+                            num = num * 10 - int.Parse(el.ToString());
+                        }
+                        else
+                        {
+                            num = num * 10 + int.Parse(el.ToString());
+                        }
+                    }
+                }
+            }
+            res += num;
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение:");
+            string expression = Console.ReadLine();
+            int result = Logic.Convert(expression);
+            Console.Write("="+result);
+        }
+    }
+}
---
+++ lab1-3Tests/LogicTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppOnlyDigit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOnlyDigit.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void ConvertTestNormal()
+        {
+            var exp = "123+5-128+66";
+            var result = Logic.Convert(exp);
+            Assert.AreEqual(66, result);
+        }
+
+        [TestMethod()]
+        public void ConvertTestZero()
+        {
+            var exp = "123+5-128+66-55-11+0";
+            var result = Logic.Convert(exp);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void ConvertTestOnlyNegative()
+        {
+            var exp = "-23-37-46";
+            var result = Logic.Convert(exp);
+            Assert.AreEqual(-106, result);
+        }
+    }
+}
