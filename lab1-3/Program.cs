0069ab0656af983e26100a7b51dbd792b33d2738
даниил дикальчук <sana.dana.81@hotmail.com>
2022-09-19 18:15:56 +0800

    Готовый вариант

--- lab1-3/Program.cs
+++ lab1-3/Program.cs
@@ -4,8 +4,21 @@ namespace ConsoleAppOnlyDigit
 {
     public class Logic
     {
+        public static string Clean(string exp)
+        {
+            string fixedExp = "";
+            foreach (var el in exp)
+            {
+                if (el == '-'|| el == '+'|| (el >= '0' && el <= '9'))
+                {
+                    fixedExp += el;
+                }
+            }
+            return fixedExp;
+        }
         public static int Convert(string exp)
         {
+            exp = Logic.Clean(exp);
             int res = 0;
             int num = 0;
             bool neg = false;
--- lab1-3Tests/LogicTests.cs
+++ lab1-3Tests/LogicTests.cs
@@ -32,5 +32,21 @@ namespace ConsoleAppOnlyDigit.Tests
             var result = Logic.Convert(exp);
             Assert.AreEqual(-106, result);
         }
+
+        [TestMethod()]
+        public void ConvertTestBadRow()
+        {
+            var exp = "-23    -37 -   4 6";
+            var result = Logic.Convert(exp);
+            Assert.AreEqual(-106, result);
+        }
+
+        [TestMethod()]
+        public void ConvertTestWorstRow()
+        {
+            var exp = "12 一冊 3    **   #! +5-1=2   8+ 五6 ﺞ = ﺞ6-55-11+0";
+            var result = Logic.Convert(exp);
+            Assert.AreEqual(0, result);
+        }
     }
 }
